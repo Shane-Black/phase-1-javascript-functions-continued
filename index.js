9f3fcc185fb87f73af1d039a031e4f0e65d413bb
Shane <shane.black@hey.com>
2023-03-09 21:17:06 +1100

    Done with lab 09/03

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(task='roller-skate') {
+    return `This Saturday, I want to ${task}!`;
+}
+
+function mondayWork(task='go to the office') {
+    return `This Monday, I will ${task}.`;
+}
+
+function wrapAdjective(string='*') {
+    return function(comment='special') {
+        return `You are ${string}${comment}${string}!`
+    }
+}
